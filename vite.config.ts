24dcfe8a6317bab51d065165b8100b37e7ada371
fasinafarook <fasinafarook786@gmail.com>
2024-10-03 07:11:48 +0530

    host

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allows access from any IP address
+  },
 })
 
 // import { defineConfig } from 'vite';
