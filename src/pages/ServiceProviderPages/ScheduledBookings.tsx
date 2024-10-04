46aa9eb780a0f29144231fbdcc80fdd55e8de089
fasinafarook <fasinafarook786@gmail.com>
2024-10-04 11:54:05 +0530

    wss

--- src/pages/ServiceProviderPages/ScheduledBookings.tsx
+++ src/pages/ServiceProviderPages/ScheduledBookings.tsx
@@ -439,6 +439,8 @@ const ScheduledBookings = () => {
             </DialogFooter>
           </Dialog>
 
+
+
           {/* Completed Modal */}
           <Dialog
             open={showCompletedModal}
