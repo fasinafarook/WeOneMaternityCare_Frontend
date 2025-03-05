76f9f97b7683309458ae7e6a993f7097533c75e2
Fasina Farook <146752536+fasinafarook@users.noreply.github.com>
2025-03-05 16:28:38 +0530

    Update axios.ts

--- src/Services/axios.ts
+++ src/Services/axios.ts
@@ -4,7 +4,7 @@ import Swal from "sweetalert2";  // Import Swal
 import { userLogout, serviceProviderLogout } from "../redux/slice/authSlice";
 
 const Api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL ||'https://we-one-maternity-care.vercel.app',
+  baseURL: import.meta.env.VITE_BASE_URL ||'https://weonematernitycare.onrender.com',
   withCredentials: true,
 });
 
